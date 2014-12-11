045a7a6083ec3432ea411bd2b90333d49549598b
Jonny Gilchrist <jonnygilchrist@gmail.com>
2014-12-11 00:26:07 +0000

    Add the player to World

--- src/engine/world.rs
+++ src/engine/world.rs
@@ -1,8 +1,10 @@
 use std::vec::Vec;
 
 use engine::Actor;
+use util::Point;
 
 pub struct World {
+    player: Actor,
     actors: Vec<Actor>,
 }
 
@@ -10,6 +12,7 @@ impl World {
 
     pub fn new() -> World {
         World {
+            player: Actor::new("Player".to_string(), Point::new(1, 1), 100, '@'),
             actors: Vec::new(),
         }
     }
