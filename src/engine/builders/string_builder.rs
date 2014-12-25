2c9a8682887b68fffc61f5a475f99b1fd06aafdb
Jonny Gilchrist <jonnygilchrist@gmail.com>
2014-12-25 20:31:44 +0000

    Implement MapFromString

--- src/engine/builders/string_builder.rs
+++ src/engine/builders/string_builder.rs
@@ -1,8 +1,7 @@
 use engine::{Map, Tile};
-use engine::builders::{MapBuilder, MapBuildResult};
-use util::units::{AsTuple, Size};
+use engine::builders::{MapBuilder, MapBuildError, MapBuildResult};
 
-use std::iter::repeat;
+use std::iter::{IteratorExt};
 
 pub struct MapFromString {
     s: String,
@@ -14,17 +13,32 @@ impl MapFromString {
         MapFromString { s: s.to_string() }
     }
 
+    fn build_line(&self, line: &str) -> Vec<Tile> {
+        line.chars().map(|char| self.build_cell(char)).collect()
+    }
+
+    fn build_cell(&self, cell: char) -> Tile {
+        match cell {
+            ' ' => Tile::Empty,
+            '.' => Tile::Floor,
+            '#' => Tile::Wall,
+            _ => { panic!("Invalid character in level string: {}", cell); }
+        }
+    }
 }
 
 impl MapBuilder for MapFromString {
     fn build(&self) -> MapBuildResult {
         let lines : Vec<_> = self.s.split('\n').collect();
 
+        if !lines.iter().all(|x| x.len() == lines[0].len()) { return Err(MapBuildError::new("Different length lines")) };
+
         println!("{}", lines);
 
-        let size = Size::new(10, 10);
-        let (width, height) = size.as_tuple();
-        let tiles: Vec<Vec<Tile>> = range(0, height).map(|_| repeat(Tile::Empty).take(width).collect()).collect();
+        let tiles: Vec<Vec<Tile>> = lines.iter().map(|&line| self.build_line(line)).collect();
+
+        println!("{}", tiles);
+
         Ok(Map::new(tiles))
     }
 }
