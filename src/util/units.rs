112b5491813580a12fd5f940e4ab56e63b0e55c5
Jonny Gilchrist <jonnygilchrist@gmail.com>
2014-12-25 02:46:35 +0000

    Add a Size type

--- src/util/units.rs
+++ src/util/units.rs
@@ -61,3 +61,22 @@ impl XYPair<int> for Direction {
         }
     }
 }
+
+pub struct Size {
+    x: uint,
+    y: uint
+}
+
+impl Size {
+
+    pub fn new(x: uint, y: uint) -> Size {
+        Size { x: x, y: y }
+    }
+
+}
+
+impl XYPair<uint> for Size {
+    fn as_tuple(&self) -> (uint, uint) {
+        (self.x, self.y)
+    }
+}
