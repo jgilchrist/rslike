02fb626eafd8bbf875841f6d539f830f4443888c
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-02-08 18:44:43 +0000

    Make the window much smaller

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use rslike::util::units::Size;
 
 fn main() {
     let game = Game::new();
-    let console = Console::new(Size::new(114, 71));
+    let console = Console::new(Size::new(80, 50));
     let mut gui = GUI::new(game, console);
 
     gui.run();
