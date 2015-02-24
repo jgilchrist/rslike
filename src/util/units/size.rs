47c3ea00514804323bdf00cf28dda74e2918bd93
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-02-24 16:06:27 +0000

    Implement Add and Sub for Size

--- src/util/units/size.rs
+++ src/util/units/size.rs
@@ -1,5 +1,7 @@
 use util::units::AsTuple;
 
+use std::ops::{Add, Sub};
+
 #[derive(Copy, Debug, PartialEq)]
 pub struct Size {
     pub x: i32,
@@ -12,6 +14,43 @@ impl Size {
         Size { x: x, y: y }
     }
 
+
+}
+
+impl Add for Size {
+    type Output = Size;
+
+    fn add(self, other: Size) -> Size {
+        let (ox, oy) = other.as_tuple();
+        Size { x: self.x + ox, y: self.y + oy }
+    }
+}
+
+impl Add<(i32, i32)> for Size {
+    type Output = Size;
+
+    fn add(self, other: (i32, i32)) -> Size {
+        let (ox, oy) = other;
+        Size { x: self.x + ox, y: self.y + oy }
+    }
+}
+
+impl Sub for Size {
+    type Output = Size;
+
+    fn sub(self, other: Size) -> Size {
+        let (ox, oy) = other.as_tuple();
+        Size { x: self.x - ox, y: self.y - oy }
+    }
+}
+
+impl Sub<(i32, i32)> for Size {
+    type Output = Size;
+
+    fn sub(self, other: (i32, i32)) -> Size {
+        let (ox, oy) = other;
+        Size { x: self.x - ox, y: self.y - oy }
+    }
 }
 
 impl AsTuple<i32> for Size {
