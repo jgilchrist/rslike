c10d7ae99319f583c65ce39da420996cb4e10b31
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-02-24 00:30:21 +0000

    Add borders for sections of the interface

--- src/gui/screens/game_screen.rs
+++ src/gui/screens/game_screen.rs
@@ -6,13 +6,17 @@ use util::units::{Direction, Point};
 #[allow(missing_copy_implementations)]
 pub struct GameScreen {
     map_location: Point,
+    info_location: Point,
+    message_location: Point,
 }
 
 impl GameScreen {
     pub fn new() -> Box<Screen> {
         Box::new(
             GameScreen {
-                map_location: Point::new(16, 1)
+                map_location: Point::new(16, 1),
+                info_location: Point::new(1, 1),
+                message_location: Point::new(16, 41),
             }
         )
     }
@@ -60,11 +64,87 @@ impl Screen for GameScreen {
 
         console.put_plain(self.map_location + pos, repr);
 
+        self.draw_borders(game, console);
     }
 }
 
 impl GameScreen {
 
+    #[allow(unused)]
+    fn draw_borders(&self, game: &mut Game, console: &mut Console) {
+        let CHAR_VLINE = 179 as char;
+        let CHAR_HLINE = 196 as char;
+        let CHAR_CORNER_NW = 218 as char;
+        let CHAR_CORNER_NE = 191 as char;
+        let CHAR_CORNER_SW = 192 as char;
+        let CHAR_CORNER_SE = 217 as char;
+
+        console.put_plain(self.map_location + Point::new(-1, -1), CHAR_CORNER_NW);
+        console.put_plain(self.map_location + Point::new(63, -1), CHAR_CORNER_NE);
+        console.put_plain(self.map_location + Point::new(-1, 38), CHAR_CORNER_SW);
+        console.put_plain(self.map_location + Point::new(63, 38), CHAR_CORNER_SE);
+
+        for x in 0..63 {
+            console.put_plain(self.map_location + Point::new(x, -1), CHAR_HLINE);
+            console.put_plain(self.map_location + Point::new(x, 38), CHAR_HLINE);
+        }
+
+        for y in 0..38 {
+            console.put_plain(self.map_location + Point::new(-1, y), CHAR_VLINE);
+            console.put_plain(self.map_location + Point::new(63, y), CHAR_VLINE);
+        }
+
+        console.put_plain(self.map_location + Point::new(0, -1), 'M');
+        console.put_plain(self.map_location + Point::new(1, -1), 'a');
+        console.put_plain(self.map_location + Point::new(2, -1), 'p');
+
+        console.put_plain(self.info_location + Point::new(-1, -1), CHAR_CORNER_NW);
+        console.put_plain(self.info_location + Point::new(13, -1), CHAR_CORNER_NE);
+        console.put_plain(self.info_location + Point::new(-1, 48), CHAR_CORNER_SW);
+        console.put_plain(self.info_location + Point::new(13, 48), CHAR_CORNER_SE);
+
+        for x in 0..13 {
+            console.put_plain(self.info_location + Point::new(x, -1), CHAR_HLINE);
+            console.put_plain(self.info_location + Point::new(x, 48), CHAR_HLINE);
+        }
+
+        for y in 0..48 {
+            console.put_plain(self.info_location + Point::new(-1, y), CHAR_VLINE);
+            console.put_plain(self.info_location + Point::new(13, y), CHAR_VLINE);
+        }
+
+        console.put_plain(self.info_location + Point::new(0, -1), 'I');
+        console.put_plain(self.info_location + Point::new(1, -1), 'n');
+        console.put_plain(self.info_location + Point::new(2, -1), 'f');
+        console.put_plain(self.info_location + Point::new(3, -1), 'o');
+
+        console.put_plain(self.message_location + Point::new(-1, -1), CHAR_CORNER_NW);
+        console.put_plain(self.message_location + Point::new(63, -1), CHAR_CORNER_NE);
+        console.put_plain(self.message_location + Point::new(-1,  8), CHAR_CORNER_SW);
+        console.put_plain(self.message_location + Point::new(63,  8), CHAR_CORNER_SE);
+
+        for x in 0..63 {
+            console.put_plain(self.message_location + Point::new(x, -1), CHAR_HLINE);
+            console.put_plain(self.message_location + Point::new(x,  8), CHAR_HLINE);
+        }
+
+        for y in 0..8 {
+            console.put_plain(self.message_location + Point::new(-1, y), CHAR_VLINE);
+            console.put_plain(self.message_location + Point::new(63, y), CHAR_VLINE);
+        }
+
+        console.put_plain(self.message_location + Point::new(0, -1), 'M');
+        console.put_plain(self.message_location + Point::new(1, -1), 'e');
+        console.put_plain(self.message_location + Point::new(2, -1), 's');
+        console.put_plain(self.message_location + Point::new(3, -1), 's');
+        console.put_plain(self.message_location + Point::new(4, -1), 'a');
+        console.put_plain(self.message_location + Point::new(5, -1), 'g');
+        console.put_plain(self.message_location + Point::new(6, -1), 'e');
+        console.put_plain(self.message_location + Point::new(7, -1), 's');
+
+    }
+
+    #[allow(unused)]
     fn render_map(&self, game: &mut Game, console: &mut Console) {
         let map = &(game.world.map);
 
