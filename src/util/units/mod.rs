91489100fe2e931eadd394511e7b4a0ecad0c6a0
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-02-27 14:42:24 +0000

    Add the Offset type

--- src/util/units/mod.rs
+++ src/util/units/mod.rs
@@ -1,9 +1,11 @@
 pub use self::direction::Direction;
+pub use self::offset::Offset;
 pub use self::point::Point;
 pub use self::rectangle::Rectangle;
 pub use self::size::Size;
 
 mod direction;
+mod offset;
 mod point;
 mod rectangle;
 mod size;
---
+++ src/util/units/offset.rs
@@ -0,0 +1,31 @@
+use util::units::{AsTuple};
+
+#[derive(Copy, Debug, PartialEq)]
+pub struct Offset {
+    pub x: i32,
+    pub y: i32
+}
+
+impl Offset {
+
+    pub fn new(x: i32, y: i32) -> Offset {
+        Offset { x: x, y: y }
+    }
+
+    pub fn zero() -> Offset {
+        Offset { x: 0, y: 0 }
+    }
+
+}
+
+impl AsTuple<i32> for Offset {
+    fn as_tuple(&self) -> (i32, i32) {
+        (self.x, self.y)
+    }
+}
+
+impl AsTuple<usize> for Offset {
+    fn as_tuple(&self) -> (usize, usize) {
+        (self.x as usize, self.y as usize)
+    }
+}
