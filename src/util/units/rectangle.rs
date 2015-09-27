c55a31de50d4623af795dad6dd8a89cc5105747f
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-09-27 20:44:14 +0100

    Add a bordered rectangle class

--- src/util/units/rectangle.rs
+++ src/util/units/rectangle.rs
@@ -34,3 +34,53 @@ impl Rectangle {
         return self.size.y;
     }
 }
+
+pub struct BorderedRectangle {
+    rect: Rectangle,
+}
+
+impl BorderedRectangle {
+    pub fn new(location: Point, size: Size) -> BorderedRectangle {
+        BorderedRectangle { rect: Rectangle::new(location, size) }
+    }
+
+    pub fn translate(&self, offset: Offset) -> BorderedRectangle {
+        BorderedRectangle { rect: self.rect.translate(offset) }
+    }
+
+    pub fn resize(&self, offset: Offset) -> BorderedRectangle {
+        BorderedRectangle { rect: self.rect.resize(offset) }
+    }
+
+    pub fn location(&self) -> Point {
+        return self.rect.location();
+    }
+
+    pub fn inner_location(&self) -> Point {
+        return self.location() + Offset::new(1, 1);
+    }
+
+    pub fn size(&self) -> Size {
+        return self.rect.size();
+    }
+
+    pub fn inner_size(&self) -> Size {
+        return self.size() - Offset::new(2, 2);
+    }
+
+    pub fn width(&self) -> i32 {
+        return self.rect.size().x;
+    }
+
+    pub fn inner_width(&self) -> i32 {
+        return self.width() - 2;
+    }
+
+    pub fn height(&self) -> i32 {
+        return self.rect.size().y;
+    }
+
+    pub fn inner_height(&self) -> i32 {
+        return self.height() - 2;
+    }
+}
